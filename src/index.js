e478206dcbaf8e3732c8b4d9f6d59f27f647f761
vvtvvtvvt <vladimir-turetskiy@mail.ru>
2017-05-09 00:36:36 +0300

    hm1: fix 6 task

--- src/index.js
+++ src/index.js
@@ -50,8 +50,7 @@ function returnFnResult(fn) {
  Функция должна принимать число (значение по умолчанию - 0) и возвращать функцию (F)
  При вызове F, переданное число должно быть увеличено на единицу и возвращено из F
  */
-function returnCounter(number) {
-    number = (number)? number : 0;
+function returnCounter(number=0) {
 
     return function () {
         return ++number
@@ -65,28 +64,9 @@ function returnCounter(number) {
  Функция должна привязать переданные аргументы к функции F и вернуть получившуюся функцию
  */
 function bindFunction(fn) {
-    var aBindArguments = {};
+    [].shift.apply(arguments);
 
-    aBindArguments.length = arguments.length-1;
-    for (var i=0; i<aBindArguments.length; ++i) {
-        aBindArguments[i] = arguments[i+1];
-    }
-
-    return function() {
-        var aResultArguments = {};
-
-        for (var i=0; i<aBindArguments.length; ++i) {
-            aResultArguments[i] = aBindArguments[i];
-        }
-        aResultArguments.length = aBindArguments.length;
-
-        for (i=0; i<arguments.length; ++i) {
-            aResultArguments[aResultArguments.length] = arguments[i];
-            ++aResultArguments.length;
-        }
-
-        return fn.apply(this, aResultArguments);
-    };
+    return fn.bind(null, ...arguments);
 }
 
 export {
